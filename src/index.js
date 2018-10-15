0483bb00554346ef501679d9fbbcf88b3d7c56ca
sonden193 <09rd193@gmail.com>
2018-10-15 20:26:06 +0900

    add simple-app-bar
    https://material-ui.com/demos/app-bar/#simple-app-bar

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SAB from './components/SimpleAppBar';
 import * as serviceWorker from './serviceWorker';
 
+ReactDOM.render(<SAB />, document.getElementById('sab'));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
